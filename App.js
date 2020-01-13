49e437bc42b8a60e6c8d83b4a15e1075b0b87c1d
Lu'lu' Al-Maknun <almaknunlulu6@gmail.com>
2020-01-13 16:53:00 +0700

    list n scroll

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'ulfi', key: '1' },
+    { name: 'angga', key: '2' },
+    { name: 'nur', key: '3' },
+    { name: 'agra', key: '4' },
+    { name: 'fatimah', key: '5' },
+    { name: 'lulu', key: '6' },
+    { name: 'tresna', key: '7' },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
+});
+
+/*
 export default function App() {
 const [name, setName] = useState("Lu'lu'");
 const [age, setAge] = useState("30");
 
-  return ( 
+  return (
     <View style={styles.container}>
       <Text>Enter Name:</Text>
-      <TextInput 
+      <TextInput
       multiline
       style={styles.input}
       placeholder='e.g. John Doe'
       onChangeText={(val) => setName(val)} />
 
       <Text>Enter Age:</Text>
-      <TextInput 
+      <TextInput
       keyboardType='numeric'
       style={styles.input}
       placeholder='e.g. 99'
@@ -41,6 +85,7 @@ const styles = StyleSheet.create({
     width: 200,
   }
 });
+*/
 
 /*
 export default function App() {
